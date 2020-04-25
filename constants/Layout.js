66873ad18ff1bc1b04be84262e9bfc7ac3accf1a
SagredoIsmael <sagredo.ismael@live.com>
2020-04-26 01:05:29 +0200

    add size normalice

--- constants/Layout.js
+++ constants/Layout.js
@@ -1,4 +1,4 @@
-import { Dimensions } from 'react-native'
+import { Dimensions, Platform, PixelRatio }from 'react-native'
 
 const width = Dimensions.get('window').width
 const height = Dimensions.get('window').height
@@ -9,4 +9,17 @@ export default {
     height,
   },
   isSmallDevice: width < 375,
-};
+}
+
+const window = Dimensions.get('window')
+const { width, height } = window
+
+const scale = width / 320
+
+export const sizeNormalize = (size) => {
+  const newSize = size * scale
+  if (Platform.OS === 'ios')
+    return Math.round(PixelRatio.roundToNearestPixel(newSize))
+  else
+   return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
+}
