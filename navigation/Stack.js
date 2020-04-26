46c9fa7fe1982a5daf17604a6966ba88d017e5f0
SagredoIsmael <sagredo.ismael@live.com>
2020-04-26 17:35:27 +0200

    hidden header

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -3,11 +3,24 @@ import { createStackNavigator } from '@react-navigation/stack'
 import Login from '../screens/Login'
 import DrawerNavigator from './Drawer'
 import { LOGIN, MAIN } from './Navigator'
+import colors from '../constants/colors'
 
 const Stack = createStackNavigator()
 
 export default () =>
-    <Stack.Navigator initialRouteName={LOGIN}>
-        <Stack.Screen name={LOGIN} component={Login} />
-        <Stack.Screen name={MAIN} component={DrawerNavigator} />
+    <Stack.Navigator initialRouteName={LOGIN} screenOptions={headerCommonsOptions}>
+        <Stack.Screen name={LOGIN} component={Login} options={headerOptions}/>
+        <Stack.Screen name={MAIN} component={DrawerNavigator} options={headerOptions}/>
     </Stack.Navigator>
+
+const headerCommonsOptions = {
+    headerStyle: {
+        backgroundColor: colors.primary,
+    },
+    headerTintColor: colors.primary,
+    headerTitleStyle: {
+        fontWeight: 'bold',
+    },
+}
+
+const headerOptions = { headerShown: false }
