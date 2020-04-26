3994e6b4811aae8413454fa56b509fe9258d3d06
SagredoIsmael <sagredo.ismael@live.com>
2020-04-26 09:54:01 +0200

    add login screen

---
+++ screens/Login.js
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import { StyleSheet, View, Text, Button } from 'react-native'
+import { MAIN } from '../navigation/Navigator'
+
+export default ({navigation}) => 
+<View style={styles.container}>
+  <Text style={styles.text}>
+    LOGIN SCREEN
+  </Text>
+  <Button
+        title="Go to next screen"
+        onPress={() => navigation.navigate(MAIN)}
+      />
+</View>
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#222',
+  },
+  text:{
+    color: 'white'
+  }
+})
+  
