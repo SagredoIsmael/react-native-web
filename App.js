42586aded2a662e1d9712db5a0a904bcf822af0d
SagredoIsmael <sagredo.ismael@live.com>
2020-04-27 10:35:56 +0200

    ignore yellow box require cycle

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import * as React from 'react'
 import Navigator from './navigation/Navigator'
+import { YellowBox } from "react-native"
 
 export default () => <Navigator/>
 
+// ignore specific yellowbox warnings
+YellowBox.ignoreWarnings(["Require cycle:"])
