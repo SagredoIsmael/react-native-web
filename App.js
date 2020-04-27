b32812b64d9700238561942d86198820e43c4c27
SagredoIsmael <sagredo.ismael@live.com>
2020-04-27 11:10:39 +0200

    change wrapper app

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import * as React from 'react'
 import Navigator from './src/navigation/Navigator'
 import { YellowBox } from "react-native"
+import { Provider } from 'react-redux'
+import configureStore from './src/redux/configureStore'
 
-export default () => <Navigator/>
+export default () => {
+    const store = configureStore()
+    return (
+        <Provider store={store}>
+            <Navigator />
+        </Provider>
+    )
+}
+
 
 // ignore specific yellowbox warnings
 YellowBox.ignoreWarnings(["Require cycle:"])
