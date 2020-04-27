a5ff7c20a6ab61f3815674a06e2598d18a294b12
SagredoIsmael <sagredo.ismael@live.com>
2020-04-27 13:37:21 +0200

    add navigation types

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -14,3 +14,10 @@ export const LOG_OUT = 'LOG_OUT'
 /*Actions locale*/
 export const SET_LOCALE = 'SET_LOCALE'
 export const SET_LANG = 'SET_LANG'
+
+/*Navigation actions*/
+export const NAVIGATE = 'NAVIGATE'
+export const GO_BACK_NAVIGATE = 'GO_BACK_NAVIGATE'
+export const RESET_NAVIGATE = 'RESET_NAVIGATE'
+export const TAB_NAVIGATE = 'TAB_NAVIGATE'
+export const TOGGLE_DRAWER = 'TOGGLE_DRAWER'
