af14f3d12f3f1acc49413a341e90ce881fbf3ccd
Ismael Sagredo <53147092+SagredoIsmael@users.noreply.github.com>
2020-05-27 23:26:17 +0200

    Add onPress (navigate) to drawer items

--- src/navigation/Drawer.js
+++ src/navigation/Drawer.js
@@ -7,14 +7,17 @@ import map from 'lodash/map'
 const Drawer = createDrawerNavigator()
 
 export default () =>
-    <Drawer.Navigator initialRouteName={MAIN} drawerContent={CustomDrawerContent}>
+    <Drawer.Navigator initialRouteName={MAIN} 
+        drawerContent={({ navigation }) => CustomDrawerContent(navigation)}>
         <Drawer.Screen name={MAIN} component={TabNavigator} />
     </Drawer.Navigator>
 
 const CustomDrawerContent = () =>
     <DrawerContentScrollView >
         {map(drawerItems, drawerItem =>
-            <DrawerItem key={drawerItem.label} {...drawerItem} />
+            <DrawerItem key={drawerItem.label}
+             onPress={() => navigation.navigate(drawerItem.label)}  
+            {...drawerItem} />
         )}
     </DrawerContentScrollView>
 
@@ -22,7 +25,6 @@ const CustomDrawerContent = () =>
 const drawerItems = [
     {
         label: "Profile",
-        onPress: function () { console.log("press Profile") }
     },
     {
         label: "Help",
